ee3f118812478b8ed7b670156128ac06a2369a6e
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-07-30 13:29:09 -0300

    update

--- front-end/src/app/dashboard/components/AcoesRapidas/AcoesRapidasMain.tsx
+++ front-end/src/app/dashboard/components/AcoesRapidas/AcoesRapidasMain.tsx
@@ -1,11 +1,13 @@
 import { Card } from "@/components/ui/card";
 import { AcoesRapidasText } from "./_components/AcoesRapidasText";
+import { AcoesRapidasCard } from "./_components/AcoesRapidasCard";
 
 export function AcoesRapidasMain() {
     return (
         <div className="">
-            <Card>
+            <Card className="p-6 bg-white shadow-md rounded-lg">
                 <AcoesRapidasText />
+                <AcoesRapidasCard />
             </Card>
         </div>
     )
---
+++ front-end/src/app/dashboard/components/AcoesRapidas/_components/AcoesRapidasCard.tsx
@@ -0,0 +1,40 @@
+import { Card } from "@/components/ui/card";
+import { ClipboardList, Monitor, AlertTriangle } from "lucide-react";
+export function AcoesRapidasCard() {
+  return (
+    <div >
+      <Card className="p-4 mb-4 bg-blue-50 shadow-md rounded-lg">
+        <div className="flex items-center space-x-4">
+          <ClipboardList className="h-6 w-6 text-blue-500" />
+          <button>
+            <h4 className="text-sm font-bold text-blue-800">
+              Nova Ordem de serviço
+            </h4>
+          </button>
+        </div>
+      </Card>
+
+      <Card className="p-4 mb-4 bg-green-50 shadow-md rounded-lg">
+        <div className="flex items-center space-x-4">
+          <ClipboardList className="h-6 w-6 text-gray-500" />
+          <button>
+            <h4 className="text-sm font-bold text-gray-800">
+              Cadastrar Equipamento
+            </h4>
+          </button>
+        </div>
+      </Card>
+
+      <Card className="p-4 shadow-md rounded-lg bg-red-50 ">
+        <div className="flex items-center space-x-4">
+          <ClipboardList className="h-6 w-6 text-gray-500" />
+          <button>
+            <h4 className="text-sm font-bold text-gray-800">
+              Relatório de Manutenção
+            </h4>
+          </button>
+        </div>
+      </Card>
+    </div>
+  );
+}
