c5a5c66ee74d2e7fbf84bb2d209c4ca7f87b43d4
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-08-01 15:32:10 -0300

    update

--- front-end/src/app/dashboard/components/Cards/_components/CardLixao.tsx
+++ front-end/src/app/dashboard/components/Cards/_components/CardLixao.tsx
@@ -1,8 +1,26 @@
-
+"use client";
+import { useEffect, useState } from "react";
 import {Trash} from 'lucide-react';
 import { Card } from '@/components/ui/card';
+import { buscarLixao } from "@/services/lixao.service";
+import { toast } from "react-toastify";
 
 export function CardLixao() {
+  const [quantidadeLixao, setQuantidadeLixao] = useState<number>(0);
+  useEffect(() => {
+    const carregarLixao = async () => {
+      try {
+        const lixao = await buscarLixao() ;
+        const total = lixao.reduce((acc, item) => acc + (item.quantidade ?? 0), 0);
+        setQuantidadeLixao(total);
+      } catch (error) {
+        console.error("Erro ao buscar lixão:", error);
+        toast.error("Erro ao buscar lixão");
+      }
+    }
+
+    carregarLixao();
+  })
   return (
     <Card className="flex items-center justify-between p-6 bg-blue-50 border border-blue-200">
       <div className="flex items-center space-x-4">
@@ -11,7 +29,7 @@ export function CardLixao() {
           <h3 className="text-lg font-semibold text-blue-800">Lixão</h3>
         </div>
       </div>
-      <span className="text-xl font-bold text-blue-800">12</span>
+      <span className="text-xl font-bold text-blue-800">{quantidadeLixao}</span>
     </Card>
   );
 }
--- front-end/src/app/dashboard/components/Cards/_components/Estoque.tsx
+++ front-end/src/app/dashboard/components/Cards/_components/Estoque.tsx
@@ -1,17 +1,39 @@
+"use client";
 
+import { useEffect, useState } from "react";
 import { Monitor } from "lucide-react";
 import { Card } from "@/components/ui/card";
+import { buscarEstoque } from "@/services/estoque.service";
+import { toast } from "react-toastify";
 
 export function CardEstoque() {
+  const [quantidadeTotal, setQuantidadeTotal] = useState<number>(0);
+
+  useEffect(() => {
+    const carregarEstoque = async () => {
+      try {
+        const estoque = await buscarEstoque();
+        // Soma todas as quantidades
+        const total = Array.isArray(estoque)? estoque.reduce((acc, item) => acc + (item.quantidade || 0), 0) : estoque.quantidade;
+        setQuantidadeTotal(total);
+      } catch (error) {
+        console.error("Erro ao buscar estoque:", error);
+        toast.error("Erro ao buscar estoque");
+      }
+    };
+
+    carregarEstoque();
+  }, []); 
+
   return (
     <Card className="flex items-center justify-between p-6 bg-green-50 border border-green-200">
       <div className="flex items-center space-x-4">
         <Monitor className="h-6 w-6 text-green-600" />
         <div>
-          <h3 className="text-lg font-semibold text-green-800">Estoque</h3>
+          <h3 className="text-lg font-semibold text-green-800">Total no Estoque</h3>
         </div>
       </div>
-      <span className="text-xl font-bold text-green-800">150</span>
+      <span className="text-xl font-bold text-green-800">{quantidadeTotal}</span>
     </Card>
   );
 }
